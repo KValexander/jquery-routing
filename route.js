d00e134e3f0e1e91de5788af551fcc4d14d69cc0
KValexander <ewoase@gmail.com>
2021-07-29 09:36:01 +0300

    upd

--- route.js
+++ route.js
@@ -1,143 +1,73 @@
-// Файл класса работы с маршрутами
-class Route {
+// Working with routes on the client side
+let route = {
+	path_to_file: "client/pages/",
+	extension: "html",
+	xhr: new XMLHttpRequest(),
 
-	// Метод инициализации класса
-	constructor(path_to_file, expansion) {
-		// Путь до папки с фалами и расширение файлов
-		this.path_to_file = path_to_file;
-		this.expansion = expansion;
-	}
+	// Modifying Path Data and Extensions
+	config: function(path, extension) {
+		this.path_to_file = path;
+		this.extension = extension;
+	},
 
-	// Метод проверки адресной строки при загрузке страницы
-	check_pathname() {
-		// Получение текущего пути
+	// Checking the address bar
+	check_pathname: function() {
 		let pathname = location.pathname;
-		// Если путь заканчивается на /, то мы его удаляем
-		if(pathname[pathname.length-1] == "/" && pathname.length > 1) pathname = pathname.replace(/.$/,"");
-
-		// Проверка пространства имён
-		let check_namespace = route.check_pathname_namespace(pathname);
-		// Если проверка обнаружила занятые имена, то прерывание исполнения дальнейшего кода
-		if(!check_namespace) return;
-
-		// Если мы не на главной странице
-		if(pathname != "/") {
-			// Переменная передаваемого пути
-			let path = "";
-			// Если путь содержит "-"
-			if(pathname.includes("-"))
-				// Убираем "/"
-				pathname = pathname.substr(1);
-			// Находим название нашего пути
-			pathname = pathname.split("-");
-			// Прогон всех элементов пути
-			for (let i = 0; i < pathname.length; i++) {
-				if(pathname.length == 1) path = pathname[i];
-				else path += "/" + pathname[i];
-			}
-			// Убираем "/"
-			path = path.substr(1);
-			// Переходим на страницу нашего пути
-			this.redirect(path);
-		// Если на главной
-		} else {
-			// Переходим на главную страницу
-			this.redirect('index');
+		if (/\/$/.test(pathname) && pathname.length > 1) pathname = pathname.replace(/.$/, "");
+		if (pathname != "/") {
+			if (pathname.includes("-")) pathname = pathname.replace("-", "/");
+			this.redirect(pathname.substr(1), false);
 		}
-	}
+		else this.redirect("index", false);
+	},
 
-	// Проверка заготовленных имён путей
-	check_pathname_namespace(pathname) {
-		// Проверка пространства имён
-		switch(pathname) {
-			// Путь ошибки авторизации
-			case("/auth_err"):
-				route.get_page("public/spa/pages/err/auth.html", "auth_err");
-				return false; break;
-			// Путь ошибки модерации
-			case("/moderation_err"):
-				route.get_page("public/spa/pages/err/moderation.html", "moderation_err");
-				return false; break;
-			// Если пространство имён ничего не обнаружило
-			default:
-				return true; break;
-		}
-	}
-
-	// Метод перенаправления на нужный маршрут
-	redirect(page, id) {
-		// Путь до страницы
-		let path = this.path_to_file + page + "." + this.expansion;
-
-		// Название url строки
+	// Redirect to the desired page
+	redirect: function(page, state) {
+		// Path and url formation
+		let path = this.path_to_file + page + "." + this.extension;
 		let url = "/" + page;
-
-		// Если есть id заявки
-		if(id != undefined) url += "/" + id;
-		// Если переходим на главную страницу
+		// Checks
 		if(page == "index") url = "/";
-
-		// Вызов метода перенаправления на нужную страницу
+		// Url setting
+		if(state == undefined) {
+			url = url.replace(/\//g,(i => m => !i++ ? m : '-')(0));
+			window.history.pushState(null, null, url); // history api
+		}
+		// Getting page
 		this.get_page(path, url);
-	}
+	},
 
-	// Получение страницы с изменением адресной строки
-	// без перезагрузки страницы
-	get_page(path, url) {
-		// Замена всех "/" на "-" кроме первого
-		// с "/" помимо первой history api корректно не работает
-		url = url.replace(/\//g,(i => m => !i++ ? m : '-')(0));
+	// Getting page
+	get_page: function(path, url) {
+		// XMLHttpRequest, the fastest
+		this.xhr.open("GET", path, false);
+		this.xhr.send();
 
-		console.log("===============================");
-		console.log("page: " + path);
-		console.log("url: " + url);
+		if (this.xhr.status == 200) {
+			if (this.xhr.responseText.includes("<!DOCTYPE html>"))
+				return route.redirect("404");
+			$("#app").html(this.xhr.responseText);
+		}
+		else console.log(this.xhr);
 
-		// Изменение маршрута адресной строки
-		window.history.pushState(null, null, url); // history api
+		// Fetch request, the shortest
+		// fetch(path).then(response => response.text())
+		// .then(data => $("#app").html(data));
+	},
 
-		// AJAX запрос
-		$.ajax({
-			url: path, // путь
-			success: function(data) {
-				// Если всё плохо и путь неправилен
-				if(data.includes("<!DOCTYPE html>")) return route.redirect("404");
-				// Загрузка данных полученной страницы
-				$("#app").html(data);
-			}
-		});
-	}
+	// Attach module
+	attach_module: function(path, elem_id) {
+		// XMLHttpRequest
+		this.xhr.open("GET", path, false);
+		this.xhr.send()
 
-	// Метод для подключения отдельных небольших файлов к странице
-	attach_module(path, element_id) {
-		console.log("module: " + path);
-		// AJAX запрос
-		$.ajax({
-			url: path, // путь
-			success: function(data) {
-				// Если всё плохо и путь неправилен
-				if(data.includes("<!DOCTYPE html>"))  {
-					return $("#" + element_id).html(`
-						<h1>Ошибка 404</h1>
-						<h3>Такого файла нет</h3>
-					`);
-				}
-				// Загрузка данных в нужный блок
-				$("#" + element_id).html(data);
-			}
-		});
-	}
+		if (this.xhr.status == 200) {
+			if (this.xhr.responseText.includes("<!DOCTYPE html>"))
+				return $("#" + element_id).html(`<h1>Ошибка 404</h1> <h3>Такого файла нет</h3>`);
+			$("#" + elem_id).html(this.xhr.responseText);
+		}
+		else console.log(this.xhr);
+	},
+};
 
-	// Метод очищения модулей
-	clear_module(element_id) {
-		$("#" + element_id).html("");
-	}
-}
-
-// Создание экземпляра класса
-let route = new Route("public/spa/pages/", "html");
-
-// Код срабатывает при загрузке страницы
-$(function() {
-	// Вызов функции проверки текущего пути
-	route.check_pathname();
-});
+window.addEventListener("popstate", e => route.check_pathname())
